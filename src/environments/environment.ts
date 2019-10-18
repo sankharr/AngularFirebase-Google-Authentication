fc0f2717627a3555abfea6c4b5a72e0a4115c4d0
Sankha Rathnayake <sankharathnayaka@gmail.com>
2019-10-18 19:51:04 +0530

    reinitiallized firebase configurations at index.html and added that to app.module.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  // firebase = {
-  //   apiKey: "AIzaSyAvMak0klYtG7A2n7C_h9GE25RQhxPSzsE",
-  //   authDomain: "angularfirebaseauth-aa856.firebaseapp.com",
-  //   databaseURL: "https://angularfirebaseauth-aa856.firebaseio.com",
-  //   projectId: "angularfirebaseauth-aa856",
-  //   storageBucket: "angularfirebaseauth-aa856.appspot.com",
-  //   messagingSenderId: "1081739377915",
-  //   appId: "1:1081739377915:web:caaa803bb95b9d4109c45e"
-  // }
+  firebase: {
+    apiKey: "AIzaSyAvMak0klYtG7A2n7C_h9GE25RQhxPSzsE",
+    authDomain: "angularfirebaseauth-aa856.firebaseapp.com",
+    databaseURL: "https://angularfirebaseauth-aa856.firebaseio.com",
+    projectId: "angularfirebaseauth-aa856",
+    storageBucket: "angularfirebaseauth-aa856.appspot.com",
+    messagingSenderId: "1081739377915",
+    appId: "1:1081739377915:web:caaa803bb95b9d4109c45e"
+  }
 };
 
 /*
