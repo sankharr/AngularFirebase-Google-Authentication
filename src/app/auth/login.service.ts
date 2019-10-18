d6d1dbacb5a3cb2f6438aa9f63ac78d6479c7bf2
Sankha Rathnayake <sankharathnayaka@gmail.com>
2019-10-18 20:53:59 +0530

    user login with google and logout created

--- src/app/auth/login.service.ts
+++ src/app/auth/login.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth
+  ) { }
+
+  login() {
+    console.log('Redirecting to Google login provider');
+    this.afAuth.auth.signInWithRedirect(new auth.GoogleAuthProvider());
+  }
+
+  getLoggedInUser() {
+    return this.afAuth.authState;
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
