5539e75b49a18d28e99855ca2314c3d34fd519bb
Sankha Rathnayake <sankharathnayaka@gmail.com>
2019-10-19 08:45:01 +0530

    finished toolbar component

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../auth/login.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  user:firebase.User;
+
+  constructor(
+    private loginService: LoginService
+  ) { }
 
   ngOnInit() {
+    this.loginService.getLoggedInUser()
+    .subscribe(user => {
+      this.user = user;
+    })
+  }
+
+  logout(){
+    this.loginService.logout();
   }
 
 }
